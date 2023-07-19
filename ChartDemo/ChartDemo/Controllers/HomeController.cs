653946743604d74ec96beefe80c22fc9c55f5576
LAPTOP-NU3RJD7C\k8854 <k8854642@gmail.com>
2023-07-19 23:18:01 +0800

    新增一條Line 並加上符號點,針對線的樣式和Bar的邊框和顏色調整

--- ChartDemo/ChartDemo/Controllers/HomeController.cs
+++ ChartDemo/ChartDemo/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Steema.TeeChart;
 using Steema.TeeChart.Styles;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -35,12 +36,59 @@ namespace ChartDemo.Controllers
             int width = 800, height = 600;
             Steema.TeeChart.TChart mChart = new TChart();
             Steema.TeeChart.Styles.Bar mBar = new Bar();
+            Steema.TeeChart.Styles.Line mLine = new Line();
+            //Steema.TeeChart.Styles.Points points1 = new Steema.TeeChart.Styles.Points(mChart.Chart);
+
+            //Color redColor = Color.Red;
+            // Color redColor = Color.FromArgb(255, 0, 0); RGB寫法
+            string hexColor = "#FF0000";
+            Color redColor = ColorTranslator.FromHtml(hexColor);
+
+            string barColor = "#FFFFE0";
+            Color yellowColor = ColorTranslator.FromHtml(barColor);
+
+
+            // 設定折線的寬度
+            //mLine.LinePen.Width = 2; // 控制折線寬度，可以調整這個值
+
+            mLine.Pointer.Style = Steema.TeeChart.Styles.PointerStyles.Circle;
+
+            // 設定符號的大小
+            //mLine.Pointer.SizeDouble = 1; // 控制符號大小，可以調整這個值
+
+            // 設定符號是否顯示數據值
+            mLine.Pointer.VertSize = 20; // 控制符號上方顯示數據值的高度
+            mLine.Pointer.InflateMargins = true; // 設定為 true，使數據點數值不與折線重疊
+
+            //折線圖的數據點是否顯示,沒有設定預設為false 不會顯示
+            mLine.Pointer.Visible = true;
+
+
+            //mLine.Pointer.Style = Steema.TeeChart.Styles.PointerStyles.Circle;
+            //// 設定符號的水平和垂直大小
+            mLine.Pointer.HorizSize = 2;
+            mLine.Pointer.VertSize = 2;
+
+            //points1.Color = redColor;
+
+            // 設定 Bar 的邊框樣式、顏色和寬度
+            mBar.Pen.Style = System.Drawing.Drawing2D.DashStyle.Solid; // 邊框樣式
+            mBar.Pen.Color = Color.Black; // 邊框顏色
+            mBar.Pen.Width = 1; // 邊框寬度
+
+            mLine.Color = redColor;
+            mBar.Color = yellowColor;
             mChart.Header.Text = "TeeChart via ImageShap PNG example";
             //將 Bar 加入 series
+            //mChart.Series.Add(points1);
             mChart.Series.Add(mBar);
+            mChart.Series.Add(mLine);
             // 內建帶入Sample 資料
+            //points1.FillSampleValues();
             mBar.FillSampleValues();
+            mLine.FillSampleValues();   
             mBar.XValues.DateTime = true; // mChart.Axes.Bottom..Lables.Angle = 90;
+            mLine.XValues.DateTime = true;
             mChart.Axes.Bottom.Increment = Steema.TeeChart.Utils.GetDateTimeStep(DateTimeSteps.OneDay);
             System.IO.MemoryStream ms = new System.IO.MemoryStream();
             mChart.Export.Image.JPEG.Width = width; 
